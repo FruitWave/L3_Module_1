77acdb73bd8b014df980fd35cfebbe1d85157f04
FruitWave <league@WorthyMini-6.attlocal.net>
2017-10-12 19:59:55 -0700

    Worked on this and 8Bit

--- src/SearchingAndSortingAlgorithms/Algorithms.java
+++ src/SearchingAndSortingAlgorithms/Algorithms.java
@@ -4,8 +4,30 @@ import java.util.List;
 
 public class Algorithms {
 	public static int findBrokenEgg(List<String> eggs) {
-		return 0; //<- this needs changing
+		for (int i = 0; i < eggs.size(); i++) {
+			if (eggs.get(i) != "whole") {
+				return i; // <- this needs changing
+			}
+		}
+		return 0;
+
 	}
-	
-	//Add other methods here
+
+	public static Object countPearls(List<Boolean> oysters) {
+		int m = 0;
+		for (int i = 0; i < oysters.size(); i++) {
+			if (oysters.get(i)) {
+				m++;
+			}
+		}
+		return m;
+	}
+
+	public static double findTallest(List<Double> peeps) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	// Add other methods here
+
 }
