4e860b9b2d50b504b3ad149c72ee10fd92cfde28
FruitWave <league@WorthyMini-6.attlocal.net>
2017-10-19 19:58:44 -0700

    Commitly de Bit.ly
    init commit we rise

--- src/SearchingAndSortingAlgorithms/Algorithms.java
+++ src/SearchingAndSortingAlgorithms/Algorithms.java
@@ -1,6 +1,7 @@
 package SearchingAndSortingAlgorithms;
 
 import java.util.List;
+import java.util.Stack;
 
 public class Algorithms {
 	public static int findBrokenEgg(List<String> eggs) {
@@ -25,9 +26,38 @@ public class Algorithms {
 
 	public static double findTallest(List<Double> peeps) {
 		// TODO Auto-generated method stub
-		return 0;
+		Stack<Double> starktowers = new Stack<>();
+		starktowers.push(peeps.get(0));
+		for (int i = 0; i < peeps.size(); i++) {
+			if (peeps.get(i) > starktowers.peek()) {
+				starktowers.push(peeps.get(i));
+			}
+		}
+		return starktowers.peek();
+
+		// Add other methods here
+
 	}
 
-	// Add other methods here
+	public static Object findLongestWord(List<String> words) {
+		Stack<String> starktowers = new Stack<>();
+		starktowers.push(words.get(0));
+		for (int i = 0; i < words.size(); i++) {
+			if (words.get(i).length() > starktowers.peek().length()) {
+				starktowers.push(words.get(i));
+			}
+		}
+		return starktowers.peek();
 
+	}
+
+	public static Object containsSOS(List<String> message) {
+		for (int i = 0; i < message.size(); i++) {
+			if (message.get(i).indexOf("... --- ...") >= 0) {
+				return true;
+			}
+		}
+		return false;
+
+	}
 }
